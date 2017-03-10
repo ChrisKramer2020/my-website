a0313c5a1909d447f46542e50eb74a2ba9a20e46
Christopher Kramer <Hendrix1957@gmail.com>
2017-03-10 17:33:42 -0500

    ck

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
+// import { Link } from 'react-router'
 import CommentBox from './CommentBox'
 class App extends Component {
   render () {
     return <div>
-      <ul id='menu'>
-        <Link to='/'><li>Home</li></Link>
-        <Link to='/'><li>Mens</li></Link>
-        <Link to='/'><li>Sale</li></Link>
-        <a href='#menu'>Open Menu</a>
-        <a href='#'>Close Menu</a>
-      </ul>
+      <section id='page-wrapper'>
+        <ul id='menu'>
+          <li id='menu-info'>Home</li>
+          <br />
+          <li id='menu-info'>Mens</li>
+          <br />
+          <li id='menu-info'>Sale</li>
+          <br />
+          <button id='menu-info'>Close Menu</button>
+          <br />
+        </ul>
+        <main id='page-content'>
+          <img src='' alt='' height='' width='' />
+        </main>
+      </section>
+      <section id='comment-form-wrapper'>
+        <form id='comment-form'>
+          <label>Join The Discussion</label>
+          <div id='comment-form-fields'>
+            <textarea placeholder='Name:' />
+            <textarea placeholder='Comment:' />
+          </div>
+          <div id='comment-form-actions'>
+            <button id='post' type='submit'>Post</button>
+          </div>
+        </form>
+      </section>
       {CommentBox}
     </div>
   }
